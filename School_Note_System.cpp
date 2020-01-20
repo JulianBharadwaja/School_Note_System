fbc322c50c297f7743548ad06bbf90b3b4408445
JulianBharadwaja <43755736+JulianBharadwaja@users.noreply.github.com>
2020-01-20 21:42:15 +0630

    add addnote, search functions

--- School_Note_System.cpp
+++ School_Note_System.cpp
@@ -3,8 +3,11 @@
 #include<string>
 #include<vector>
 using namespace std;
+
+
 void AddStudent(){
 	string temp;
+	vector<string>Data;
 	cout << "Enter Student's First Name: ";
 	cin >> temp;
 	Data.push_back(temp);
@@ -26,6 +29,68 @@ void AddStudent(){
 	file << Data[2] << endl;
 	file << Data[3] << endl;
 	file.close();
+}
+void AddNote(){
+	vector<string> Subject;
+	vector<string> Note;
+	string temp;
+	string student;
+	int z = 0;
+	cout << "Enter student ID to add note: ";
+	cin >> student;
+	while(true){
+		cout << "\nEnter subject: "; 
+		cin >> temp;
+		Subject.push_back(temp);
+		cout << "Enter note: ";
+		cin >> temp;
+		Note.push_back(temp);
+		cout << "Do you want to continue?[0]Yes[1]No ";
+		cin >> z; 
+		if(z!=0){
+			break;
+		}
+	}
+	string fileName;
+	fileName = student + ".cal";
+	ofstream file;
+	file.open(fileName.c_str());
+	for(int i = 0; i != Subject.size() ;i++){
+		
+	}
+	file.close();
+}
+void SearchStudent(){
+	string number;
+	cout << "Insert student ID: ";
+	cin >> number;
+	string fileName;
+	fileName = number+".dat";
+	ifstream file;
+	file.open(fileName.c_str());
+	vector<string>data;
+	if(file.is_open()){
+		file << Subject[i] << " " << Note [i] << endl;
+	string in;
+	while(!file.eof()){
+		file >> in;
+		data.push_back(in);
+	}
+	cout << "First Name: " << data[0] << endl;
+	cout << "Last Name: " << data[1] << endl;
+	cout << "Phone Number: " << data[2] << endl;
+	cout << "Student Number: "<< data[3] << endl;
+	file.close();
+	}else {
+		
+	}
+	
+}
+void ReadKardex(){
+	
+}
+void PrintKardex(){
+	
 }
 int main(){
 	int sel = 0;
